9c12eef29da2f5958566a6003702df7f8c3da303
oliveratgithub <github@raduner.ch>
2024-12-24 15:16:22 +0100

    Adds zorg-specific config, questions, and strings

--- strings.py
+++ strings.py
@@ -27,6 +27,13 @@ help = [
 unknowncmd = '... wat.'
 
 # Message strings - customize these as you desire
-question = 'TOPIC: %s - Q: %s'
-botsnack = "I'm hungry. Please feed me with !botsnack."
-#...
+question = '<%s> Q: %s'
+botsnack = "Jetzt bin ich hungrig. Fütter mich mit !botsnack damit es weitergeht"
+thanks = 'yummy. tanketuusig! :-)'
+waiting = 'Allei isches nöd so lustig. Warte mer uf meh Spieler...'
+hintone = 'Nur 1 Silbe'
+rightanswer = 'Sigh. d Antwort wär gsii "%s"'
+wishluck = 'loosers.'
+correctanswer = 'Riiiischtisch! "%s" stimmt :) congratz %s!'
+winner = '%s, lass dich huldigen - du hesch abgruumt!!!'
+channeltopic = 'Do wird quizzd! >> Winner score: %d | Last Winner: %s | All-time Champ: %s (%d)'
